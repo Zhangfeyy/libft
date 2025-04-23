c6f62d7afc7ab6fc2b4dfd327aa9edbe8cb60e4b
Feiya Zhang <fzhang@c1r2p10.42vienna.com>
2025-04-23 13:09:37 +0200

    added

---
+++ ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fzhang <fzhang@student.42vienna.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/23 12:55:25 by fzhang            #+#    #+#             */
+/*   Updated: 2025/04/23 12:55:27 by fzhang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <string.h>
+void	*ft_memchr(const void	*s, int	c, size_t	n) {
+	unsigned char	*copy;
+	size_t	i;
+
+	copy = s;
+	i = 0;
+	while (i < n)
+	{
+		if (*copy == c)
+			return (copy);
+		copy++;
+		i++;
+	}
+	return (NULL);
+}
---
+++ ft_strlcat.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fzhang <fzhang@student.42vienna.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/23 12:09:15 by fzhang            #+#    #+#             */
+/*   Updated: 2025/04/23 12:09:17 by fzhang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <string.h>
+
+unsigned int    ft_strlcat(char   *dst, const char    *src, size_t    size) {
+    unsigned int    i;
+    unsigned int    j;
+    unsigned int    k;
+    
+    i = 0;
+    j = 0;
+    while(!dst[j]) {
+        j++;
+    }
+    while((i < size) && (!src[i]))
+    {
+        dst[j] = src[i];
+        j++;
+        i++;
+    }
+    k = j;
+    while(i < size)
+    {
+        dst[j] = '\0';
+        j++;
+        i++;
+    }
+    return (k);
+}
