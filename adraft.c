73509950a45edc78489ba80293d74b7bfcf04978
Feiya Zhang <fzhang@c2r6p6.42vienna.com>
2025-04-29 16:31:35 +0200

    added

--- adraft.c
+++ adraft.c
@@ -67,3 +67,57 @@ void ft_lstdelone(t_list *lst, void (*del)(void *))
     del(lst->content);
     free(lst);
 }
+
+void ft_lstclear(t_list **lst, void (*del)(void*))
+{
+    t_list *temp;
+
+    temp = *lst;
+    while(temp)
+    {
+        temp = (*lst)->next;
+        (*lst)->next = temp->next;
+        ft_lstdelone(temp, del);
+    }
+    temp = *lst;
+    *lst = NULL;
+    ft_lstdelone(temp, del);
+}
+
+void ft_lstiter(t_list *lst, void (*f)(void *))
+{
+    t_list *temp;
+
+    temp = lst;
+    while(temp)
+    {
+        f(temp->content);
+        temp = temp->next;
+    }
+}
+
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+    t_list *temp_old;
+    t_list *temp_new;
+    t_list *new;
+
+    temp_old = lst;
+    new = ft_lstnew(f(temp_old->content));
+    if(!new)
+        return(NULL);
+    temp_new = new;
+    temp_old = temp_old->next;
+    while(temp_old)
+    {
+        temp_new->next = ft_lstnew(f(temp_old->content));
+        if(!temp_new->next)
+        {
+            ft_lstclear(&new, del);
+            free(new);
+            return (NULL);
+        }
+        temp_new = temp_new->next;
+        temp_old = temp_old->next;
+    }
+}
